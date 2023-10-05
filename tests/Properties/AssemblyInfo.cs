6cddfa65dc696c4af8bbfa82d5ab41ece9ae79bd
dennisvg111 <dennisvg111@gmail.com>
2023-10-05 15:17:04 +0200

    Initial commit

---
+++ tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c88e68e7-e060-4800-bcf8-98c631f3c809")]
