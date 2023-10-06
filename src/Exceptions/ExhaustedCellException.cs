b40b79f38287044b679a742ce09245b5e836a82e
dennisvg111 <dennisvg111@gmail.com>
2023-10-06 17:19:31 +0200

    Remove unused property

--- src/Exceptions/ExhaustedCellException.cs
+++ src/Exceptions/ExhaustedCellException.cs
@@ -3,13 +3,10 @@ using System.Runtime.Serialization;
 
 namespace DG.Sudoku.Exceptions
 {
-
     [Serializable]
     public class ExhaustedCellException : Exception
     {
-        public Cell Cell { get; set; }
-
-        public ExhaustedCellException(Cell cell, string message) : base($"[Cell {cell.X + 1},{cell.Y + 1}] " + message)
+        public ExhaustedCellException(string message) : base(message)
         {
 
         }
