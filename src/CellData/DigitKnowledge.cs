9398d35d9d3335a432e1c5946b81cde41db16097
dennisvg111 <dennisvg111@gmail.com>
2023-10-07 15:13:43 +0200

    Add comments to the DigitKnowledge class

--- src/CellData/DigitKnowledge.cs
+++ src/CellData/DigitKnowledge.cs
@@ -1,12 +1,23 @@
 ﻿namespace DG.Sudoku.CellData
 {
     /// <summary>
-    /// Indicates if this digit is a regular digit, a guessed digit, or given by the puzzle.
+    /// Indicates if this digit is an unknown digit, a guessed digit, or given by the puzzle.
     /// </summary>
     public enum DigitKnowledge
     {
+        /// <summary>
+        /// This digit is not yet known for sure.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// This digit is given.
+        /// </summary>
         Given,
+
+        /// <summary>
+        /// This digit is guessed.
+        /// </summary>
         Guessed
     }
 }
