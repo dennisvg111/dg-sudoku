f3bef44bded9ec9085d2e8e7861c819aff659d1e
dennisvg111 <dennisvg111@gmail.com>
2023-10-08 16:17:44 +0200

    Add comments to interfaces, difficulty

--- src/SolvingStrategies/ISolvingStrategy.cs
+++ src/SolvingStrategies/ISolvingStrategy.cs
@@ -2,10 +2,26 @@
 
 namespace DG.Sudoku.SolvingStrategies
 {
+    /// <summary>
+    /// Defines a strategy used to remove possible digits from cells in a sudoku board.
+    /// </summary>
     public interface ISolvingStrategy
     {
+        /// <summary>
+        /// The name of this strategy.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// How difficult this strategy would be to use.
+        /// </summary>
         Difficulty Difficulty { get; }
+
+        /// <summary>
+        /// Finds the values to remove from the board. 
+        /// </summary>
+        /// <param name="board"></param>
+        /// <returns></returns>
         IEnumerable<PossibleDigitInCell> FindValuesToRemove(Board board);
     }
 }
