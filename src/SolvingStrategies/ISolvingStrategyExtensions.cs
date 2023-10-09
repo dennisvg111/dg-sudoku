888684d4f4529b50884cbc1bffece485294f3e24
dennisvg111 <dennisvg111@gmail.com>
2023-10-09 15:22:44 +0200

    Add comments to ISolvingStrategyExtensions class

--- src/SolvingStrategies/ISolvingStrategyExtensions.cs
+++ src/SolvingStrategies/ISolvingStrategyExtensions.cs
@@ -3,8 +3,18 @@ using System.Linq;
 
 namespace DG.Sudoku.SolvingStrategies
 {
+    /// <summary>
+    /// Provides extension methods for classes implementing <see cref="ISolvingStrategy"/>.
+    /// </summary>
     public static class ISolvingStrategyExtensions
     {
+        /// <summary>
+        /// Executes the <see cref="ISolvingStrategy.FindValuesToRemove(Board)"/> method and returns a value indicating if any digits have been found that haven't already been removed.
+        /// </summary>
+        /// <param name="strategy"></param>
+        /// <param name="board"></param>
+        /// <param name="valuesToRemove"></param>
+        /// <returns></returns>
         public static bool TryFindValuesToRemove(this ISolvingStrategy strategy, Board board, out IEnumerable<PossibleDigitInCell> valuesToRemove)
         {
             var copy = board.Copy();
