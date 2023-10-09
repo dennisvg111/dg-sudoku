162c40c460850e383438dbe81f1eff01b5d773af
dennisvg111 <dennisvg111@gmail.com>
2023-10-09 09:10:08 +0200

    Remove ;

--- console/Program.cs
+++ console/Program.cs
@@ -28,7 +28,7 @@ namespace DG.Sudoku.Console
                 DrawDetailedBoard(board);
                 Thread.Sleep(500);
             } while (solver.NextStep(board));
-            Output.WriteLine("Could not find more solving steps"); ;
+            Output.WriteLine("Could not find more solving steps");
         }
 
         private static void DrawDetailedBoard(Board board)
