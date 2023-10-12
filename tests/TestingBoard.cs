a8699194402ad2a9e7c3744cfe15260af52f6095
dennisvg111 <dennisvg111@gmail.com>
2023-10-12 16:48:05 +0200

    Add tests to Board class, clean up CellDigit tests

---
+++ tests/TestingBoard.cs
@@ -0,0 +1,18 @@
+﻿namespace DG.Sudoku.Tests
+{
+    public class TestingBoard : Board
+    {
+        public new Cell this[int x, int y]
+        {
+            get
+            {
+                return base[x, y];
+            }
+            set
+            {
+                var index = ConvertToArrayIndex(x, y);
+                _cells[index] = value;
+            }
+        }
+    }
+}
